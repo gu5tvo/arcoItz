797a138e00786bb817257c09e502c9b202f4954e
EdigarCarvalho <edgarcarvalho8989@gmail.com>
2023-04-23 16:04:12 -0300

    feat: uso de verifyShape e criação do middleware checkUserExistence

--- backend/src/routes/deleteRouter.ts
+++ backend/src/routes/deleteRouter.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express'
 import checkCredentialsMiddleware from '../middleware/delete/checkCredentials.middleware'
 import deleteController from '../controllers/delete.controller'
+import { deleteSchema } from '../schemas/delete.schema'
+import verifyShape from '../schemas/verifyShape.middleware'
+import checkUserExistenceMiddleware from '../middleware/delete/checkUserExistence.middleware'
 
 const deleteRouter = Router()
 
-deleteRouter.delete('', checkCredentialsMiddleware, deleteController)
+deleteRouter.delete('', verifyShape(deleteSchema), checkCredentialsMiddleware, checkUserExistenceMiddleware, deleteController)
 
 export default deleteRouter
