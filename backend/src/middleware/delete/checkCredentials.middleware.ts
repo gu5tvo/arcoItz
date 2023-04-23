9a7dad8d46e0f58ee2bbff9ced941c5b006bea41
Raspincel <raspincel@gmail.com>
2023-04-23 20:33:43 -0300

    fix: remoção de console.log()'s

--- backend/src/middleware/delete/checkCredentials.middleware.ts
+++ backend/src/middleware/delete/checkCredentials.middleware.ts
@@ -4,13 +4,11 @@ import jwt from 'jsonwebtoken'
 
 export default async function checkCredentialsMiddleware(req: Request, res: Response, next: NextFunction){
     const { id } = req.params
-    
-    console.log(req.params)
 
     const { token } = req.body
+
     const { id: tokenId } = await jwt.verify(token, process.env.SECRET_KEY)   
 
-    console.log(tokenId)
     if (tokenId != id) throw new AppError("Credenciais para deleção inválidas", 403)
 
     // Armazena o id para ser usado no controller
