e585a7cd0689fab9d1473cda572d17e86441b490
Raspincel <raspincel@gmail.com>
2023-04-23 18:41:10 -0300

    fix: uso de req.params no middleware

--- backend/src/middleware/editProfile/checkCredentials.middleware.ts
+++ backend/src/middleware/editProfile/checkCredentials.middleware.ts
@@ -5,15 +5,15 @@ import jwt from 'jsonwebtoken'
 export default async function checkCredentialsMiddleware(req: Request, res: Response, next: NextFunction){
     
     // Extrai o id do path da requisição
-    const path = req.originalUrl.split('/')[2]
+    const { id } = req.params
     
     const { token } = req.body
-    const { id } = await jwt.verify(token, process.env.SECRET_KEY)
+    const { tokenId } = await jwt.verify(token, process.env.SECRET_KEY)
 
-    if (path != id) throw new AppError("Credenciais para edição de perfil inválidas", 403)
+    if (tokenId != id) throw new AppError("Credenciais para edição de perfil inválidas", 403)
 
     // Armazena o id para ser usado no controller
-    req.user = id
+    req.user.id = id
 
     next()
 }
