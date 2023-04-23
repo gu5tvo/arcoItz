5b11a9b8c67536bb705f472aa31cf5d1c0a5665a
Raspincel <raspincel@gmail.com>
2023-04-23 19:09:05 -0300

    hotfix: removidas menções a listUsers

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,7 +7,6 @@ import { errorHandler } from './errors'
 import loginRouter from './routes/loginRouter'
 import registerRouter from './routes/registerRouter'
 import deleteRouter from './routes/deleteRouter'
-import listUsersRouter from './routes/listUsersRouter'
 
 const app = express()
 app.use(cors())
@@ -17,7 +16,6 @@ app.use(express.json())
 app.use('/login', loginRouter)
 app.use('/register', registerRouter)
 app.use('/delete/:id', deleteRouter)
-app.use('/listUsers', listUsersRouter)
 
 app.use(errorHandler)
 export default app;
