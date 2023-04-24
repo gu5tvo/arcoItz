fa9cb778e4c10e928d962a4f50ff2c04d0b88ffb
EdigarCarvalho <edgarcarvalho8989@gmail.com>
2023-04-24 00:33:46 -0300

    feat: rota de busca por um perfil específico de usuário

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,6 +8,7 @@ import loginRouter from './routes/loginRouter'
 import registerRouter from './routes/registerRouter'
 import deleteRouter from './routes/deleteRouter'
 import editProfileRouter from './routes/editProfileRouter'
+import specificProfileRouter from './routes/specificProfileRouter'
 
 const app = express()
 app.use(cors())
@@ -18,6 +19,8 @@ app.use('/login', loginRouter)
 app.use('/register', registerRouter)
 app.use('/delete', deleteRouter)
 app.use('/editProfile', editProfileRouter)
+app.use('/specificProfile', specificProfileRouter)
+
 
 app.use(errorHandler)
 export default app;
--- backend/src/routes/specificProfileRouter.ts
+++ backend/src/routes/specificProfileRouter.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-
+import checkUserExistenceMiddleware from "../middleware/specificProfile/checkUserExistence.middleware";
+import specificProfileController from "../controllers/specificProfile.controller";
 
 const specificProfileRouter = Router()
 
-specificProfileRouter.get('')
+specificProfileRouter.get('/:id', checkUserExistenceMiddleware, specificProfileController)
 
 export default specificProfileRouter
