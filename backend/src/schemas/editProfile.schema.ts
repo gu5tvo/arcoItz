3c83e6b5e0960c8aa6cea7f4f4ca0c5f23ac508e
Raspincel <raspincel@gmail.com>
2023-04-23 21:50:35 -0300

    feat: adicionado serializer para validação de saída de dados

--- backend/src/schemas/editProfile.schema.ts
+++ backend/src/schemas/editProfile.schema.ts
@@ -2,6 +2,21 @@ import * as yup from 'yup'
 
 // Esquemas de validação de ENTRADA
 export const editProfileSchema = yup.object().shape({
+    token: yup.string().required(),
+    id: yup.string().required(),
+    avatar: yup.string(),
+    name: yup.string().required(),
+    email: yup.string().required().email(),
+    bio: yup.string(),
+    area: yup.string(),
+    address: yup.string(),
+    title: yup.string(),
+    gender: yup.string(),
+    pronouns: yup.string(),
+})
+
+// Esquemas de validação de SAÍDA
+export const editProfileSerializer = yup.object().shape({
     id: yup.string().required(),
     avatar: yup.string(),
     name: yup.string().required(),
