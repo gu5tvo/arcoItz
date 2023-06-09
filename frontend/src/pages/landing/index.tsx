2f3661b08880c5a0db23b08413b069f51079a417
Luciano <lucianoamorim506@gmail.com>
2023-06-09 20:14:40 -0300

    fix: melhoria da responsividade, ajuste para se assemelhar mais ao protótipo

--- frontend/src/pages/landing/index.tsx
+++ frontend/src/pages/landing/index.tsx
@@ -11,42 +11,52 @@ export default function LandingPage(): JSX.Element{
         <>
             <DinamicHeader loginBtn={true} registerBtn={true} searchBtn={true}/>
             <PinkSection>
-                <div>
-                    <h2>Trabalhar é um direito de todes.</h2>
-                    <small>Apoie essa causa, clique em ”saiba mais”.</small>
-                    <Link to="/about">Saiba mais</Link>
+                <div className="content">
+                    <div className="cta">
+                        <h2>Trabalhar é um direito de todes.</h2>
+                        <small> Apoie essa causa, clique em ”saiba mais”.</small>
+                        <Link to="/about">Saiba mais</Link>
+                    </div>
+                    <img src={splash} alt="Mulher trans segurando a bandeira Trans, com o mapa do maranhão sobreposto com as cores da bandeira do maranhão."/>
                 </div>
-                <img src={splash} alt="Mulher trans segurando a bandeira Trans, com o mapa do maranhão sobreposto com as cores da bandeira do maranhão."/>
             </PinkSection>
             <DisclaimerSection>
-                <img src={disclaimer} alt="Mão erguida ao ar, pintada com as cores LGBT, representando uma luta por reivindicação de direitos." />
-                <div>
-                    <h3>Promovendo empregabilidade</h3>
-                    <p>
-                    Buscamos promover um ambiente onde as pessoas possam cadastrar-se e deixar visível seu currículo. Nosso foco principal é auxilar as pessoas a ingressarem no mercado de trabalho de maneira simplificada, e sem burocracia. 
-                    </p>
+                <div className="content">
+                    <img src={disclaimer} alt="Mão erguida ao ar, pintada com as cores LGBT, representando uma luta por reivindicação de direitos." />
+                    <div className="description">
+                        <h3>Promovendo empregabilidade</h3>
+                        <p>
+                        Buscamos promover um ambiente onde as pessoas possam cadastrar-se e deixar visível seu currículo. Nosso foco principal é auxilar as pessoas a ingressarem no mercado de trabalho de maneira simplificada, e sem burocracia. 
+                        </p>
+                    </div>
                 </div>
             </DisclaimerSection>
             <ActionSection>
                 <BlockArticle>
-                    <h4>É um recrutador?</h4>
-                    <p>
-                    Recrutador, encontre os melhores talentos para sua empresa buscando currículos em nosso site. Cadastre-se agora e encontre o candidato ideal.
-                    </p>
+                    <div>
+                        <h4>É um recrutador?</h4>
+                        <p>
+                        Recrutador, encontre os melhores talentos para sua empresa buscando currículos em nosso site. Cadastre-se agora e encontre o candidato ideal.
+                        </p>
+                    </div>
                     <Link to="/search">Ver currículos</Link>
                 </BlockArticle>
                 <BlockArticle>
-                    <h4>É um candidato?</h4>
-                    <p>
-                    Seja visto pelos melhores empregadores! Cadastre seu currículo em nosso site e aumente suas chances de conquistar seu emprego dos sonhos.
-                    </p>
+                    <div>
+                        <h4>É um candidato?</h4>
+                        <p>
+                        Seja visto pelos melhores empregadores! Cadastre seu currículo em nosso site e aumente suas chances de conquistar seu emprego dos sonhos.
+                        </p>
+                    </div>
                     <Link to="/register">Cadastre-se</Link>
                 </BlockArticle>
                 <BlockArticle>
-                    <h4>Saiba sobre nossa causa</h4>
-                    <p>
-                    Saiba mais a respeito dos nossos objetivos de maneira mais detalhada clicando no botão abaixo.
-                    </p>
+                    <div>
+                        <h4>Saiba sobre nossa causa</h4>
+                        <p>
+                        Saiba mais a respeito dos nossos objetivos de maneira mais detalhada clicando no botão abaixo.
+                        </p>
+                    </div>
                     <Link to="/about">Saiba mais</Link>
                 </BlockArticle>
             </ActionSection>
--- frontend/src/pages/landing/styles.ts
+++ frontend/src/pages/landing/styles.ts
@@ -2,22 +2,36 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 export const PinkSection = styled.section`
-    background-color: #FFCCE4;
-    padding: 0 2em;
+    background-color: ${props => props.colors};
     display: flex;
     flex-direction: row;
-    justify-content: space-between;
-    height: 45svh;
-    font-family: 'Roboto', sans-serif;
+    justify-content: center;
+    font-family: 'Inter', sans-serif;
     text-align: center;
+    position: relative;
+    
+    .content { 
+        width: min(100vw, 2200px);
+        position: relative;
+        display: flex;
+        justify-content: space-around;
+    }
 
     @media (max-width: 768px){
+
+        padding: 2rem;
+
         img{
             display: none;
         }
+
+        .content {
+            justify-content: center;
+            
+        }
     }
 
-    div{
+    .cta{
         display: flex;
         flex-direction: column;
         justify-content: center;
@@ -26,20 +40,26 @@ export const PinkSection = styled.section`
     }
 
     h2{
-        font-size: 2em;
-        font-weight: 700;
+        font-size: clamp(1.7rem, 2vw, 2.4rem);
+        font-weight: 900;
         width: 60%;
         color: #0A2A5E;
     }
+    
     small{
-        font-size: 0.8em;
-        font-weight: 400;
+        padding-top: 0.6em;
+        padding-bottom: 0.6em;
+        font-size: clamp(0.8rem, 1vw, 1.2rem);
+        font-weight: 600;
         color: #0A2A5E;
     }
 
     img{
-        position: inherit;
-        margin-bottom: -5.5rem;
+        position: relative;
+        bottom: 0;
+        max-width: 30%;
+        width: 100vw;
+        margin-left: 4vw;        
     }
 
     a{
@@ -50,6 +70,7 @@ export const PinkSection = styled.section`
         transition: all 0.3s ease-in-out;
         border-radius: 0.5em;
         margin-top: .5em;
+        font-size: clamp(0.95rem, 0.95vw, 1.2rem);
 
         &:hover{
             cursor: pointer;
@@ -59,14 +80,24 @@ export const PinkSection = styled.section`
 `
 
 export const DisclaimerSection = styled.section`
-    background-color: #FFFFFF;
-    padding: 1em 2em;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-evenly;
-    align-items: center;
+    width: 100vw;
 
-    div{
+    background-color: #FFFFFF;
+    padding: 1rem 2rem;    
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+    .content {
+        width: min(100vw, 1700px);
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+        align-items: center;
+    }
+
+    .description{
         width: 30%;
     }
 
@@ -76,13 +107,30 @@ export const DisclaimerSection = styled.section`
         font-weight: 700;
     }
 
+    p {
+        text-align: left;
+        line-height: 1.3rem;
+    }
+
     @media (max-width: 768px){
-        div{
-            width: 100%;
-            padding: 1em;
+
+        padding: 2rem 0;
+
+        .description{
+            text-align: center;
+            width: 80%;
         }
+
         img{
             display: none;
+        }
+
+        .content {
+            
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
         }
     }
 `
@@ -91,19 +139,16 @@ export const ActionSection = styled.section`
     padding: 3em;
     display: flex;
     flex-direction: row;
-    justify-content: space-around;
+    flex-wrap: wrap;
+    justify-content: center;
     align-items: center;
-
-    @media (max-width: 768px){
-        flex-direction: column;
-        gap: 2em;
-    }
+    gap: min(4rem, 7vw);
 `
 
 export const BlockArticle = styled.article`
     display: flex;
     flex-direction: column;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
     gap: 1em;
     width: 240px;
@@ -114,21 +159,33 @@ export const BlockArticle = styled.article`
     outline: 1px solid #48A7FF;
     transition: all 0.3s ease-in-out;
     box-shadow: 0 0 0 #48A7FF;
-
+    text-align: left;
+    
     &:hover{
         scale: 1.05;
         box-shadow: 0 0 .6em #48A7FF;
     }
 
 
-    h4{
-        font-size: 1.2em;
+    div h4{
+        font-size: 1.1em;
         font-weight: 700;
+        margin-bottom: 1rem;
     }
 
-    p{
+    div p{
         text-rendering: optimizeLegibility;
-        width: 80%;
+    }
+
+    div {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;    
+        gap: 0.7rem;
+        line-height: 1.2rem;
+        padding: 0.8rem;
+        padding-bottom: 0;
     }
 
     a{
