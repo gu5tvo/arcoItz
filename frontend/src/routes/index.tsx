026b6122b29d6959f379b2995688512ba5cfa37d
EdigarCarvalho <edgarcarvalho8989@gmail.com>
2023-06-29 01:32:41 -0300

    feat: add new routes

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -6,6 +6,9 @@ import RegisterPage from '../pages/register';
 import UserDashboardPage from '../pages/dashboard';
 import SearchPage from '../pages/search';
 import ProtectedRoute from '../components/ProtectedRoute';
+import Profile from '../pages/profile';
+import { Painel } from '../pages/painel';
+import Cities from '../pages/cities';
 
 export default function MakeRoutes(){
 
@@ -16,6 +19,9 @@ export default function MakeRoutes(){
             <Route path="/register" element={<RegisterPage/>} />
             <Route path="/search" element={<SearchPage/>} />
             <Route path="/l" element={<UserDashboardPage/>} />
+            <Route path="/painel" element={<Painel/>} />
+            <Route path="/cities" element={<Cities/>} />
+            <Route path="/profile/:id" element={<Profile/>} />
             <Route path="/dashboard/*" element={<ProtectedRoute path="/dashboard" element={UserDashboardPage} isPrivate redirectTo="/login" />} />
         </Routes>
     )
