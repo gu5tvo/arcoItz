16e76f08c5247451b83f359a2f95d42c57c32fc6
Ismael Aquino <ismaellAquino@hotmail.com>
2023-09-15 11:16:01 -0300

    feat: add dynamic header on profile

--- frontend/src/pages/profile/index.tsx
+++ frontend/src/pages/profile/index.tsx
@@ -8,6 +8,7 @@ import pdfIcon from '../../assets/icon_PDF.svg'
 import linkedinIcon from '../../assets/linkedin.svg'
 import instagramIcon from '../../assets/instagram.svg'
 import portifolioIcon from '../../assets/portifolio.svg'
+import DynamicHeader from '../../layouts/Header';
 
 export default function Profile() {
   const { displayProfile } = useUser();
@@ -57,126 +58,129 @@ export default function Profile() {
   }, [displayProfile, id]);
   
   return (
-    <ContainerProfile>
-      <div className='content'>
-        <UserGradient>
-          <div className='profile-header'>
-            <img src={user?.avatar ? user?.avatar : DefaultPicture } alt={`imagem de ${user?.name}`} />
-            <div className='profile-description'>
-              <h3>{user?.pronouns}</h3>
+    <>
+      <DynamicHeader startBtn></DynamicHeader>
+      <ContainerProfile>
+        <div className='content'>
+          <UserGradient>
+            <div className='profile-header'>
+              <img src={user?.avatar ? user?.avatar : DefaultPicture } alt={`imagem de ${user?.name}`} />
+              <div className='profile-description'>
+                <h3>{user?.pronouns}</h3>
+                <hr className="desktop-hr"/>
+                <div className='profile-info'>
+                  <h1>{user?.name}</h1>
+                  <h3>{user?.title}</h3>
+                </div>
+              </div>
+            </div>
+            <hr className="mobile-hr"/>
+            <div className='profile-contact'>
+              <h2>Contato</h2>
               <hr className="desktop-hr"/>
               <div className='profile-info'>
-                <h1>{user?.name}</h1>
-                <h3>{user?.title}</h3>
+                <h3>{user?.email}</h3>
+                <h3>{user?.phone}</h3>
+                <div className='social-media'>
+                { user?.linkedin && <a href={user?.linkedin}><img src={linkedinIcon}/></a> } 
+                { user?.portfolio && <a href={user?.portfolio}><img src={portifolioIcon}/></a> }
+                { user?.instagram && <a href={user?.instagram}><img src={instagramIcon}/></a>}
+                </div>
               </div>
             </div>
-          </div>
-          <hr className="mobile-hr"/>
-          <div className='profile-contact'>
-            <h2>Contato</h2>
-            <hr className="desktop-hr"/>
-            <div className='profile-info'>
-              <h3>{user?.email}</h3>
-              <h3>{user?.phone}</h3>
-              <div className='social-media'>
-              { user?.linkedin && <a href={user?.linkedin}><img src={linkedinIcon}/></a> } 
-              { user?.portfolio && <a href={user?.portfolio}><img src={portifolioIcon}/></a> }
-              { user?.instagram && <a href={user?.instagram}><img src={instagramIcon}/></a>}
+          </UserGradient>
+
+          <UserInfos>
+            <span className="basic-presentation">
+              <h1>{user?.name}</h1>
+              <p>{user?.title}</p>
+            </span>
+
+            <span className="infos-section">
+              <h2>Informações pessoais:</h2>
+              <hr/>
+              <div className='user-bio'>
+                <p>{user?.bio}</p>
               </div>
-            </div>
-          </div>
-        </UserGradient>
+            </span>
 
-        <UserInfos>
-          <span className="basic-presentation">
-            <h1>{user?.name}</h1>
-            <p>{user?.title}</p>
-          </span>
-
-          <span className="infos-section">
-            <h2>Informações pessoais:</h2>
-            <hr/>
-            <div className='user-bio'>
-              <p>{user?.bio}</p>
-            </div>
-          </span>
-
-          <span className="infos-section">
-            <h2>Experiências profissionais:</h2>
-            <hr/>
-                 
-                                        
-            {
-              user?.experiences.map((experience) => (
-                <div key={experience.id} className="experience">
-                <h3>{experience.title}<span>{experience.from} — {experience.to}</span></h3>
-                <span className={`wrapper ${openAccordionList.includes(experience.id) ? 'open' : ''}`}>
-                  <div className="details">
-                    <h4>Companhia: <span>{experience.company}</span></h4>
-                    <p>{experience.description}</p>
-                  </div>
-                  </span>
-                  <span onClick={()=>handleAccordionClick(experience.id)} className="clicker">+</span>
-                </div>
-              ))
-            }
-          </span>
-
-          <span className="infos-section">
-            <h2>Formações Acadêmicas:</h2>
-            <hr/>
-            {
-              user?.courses.map((course) => {
-                
-                return <div key={course.id} className="experience">
-                  <h3>{course.name}<span>{course.from} { course.to && `— ${course.to}`}</span></h3>
-                  <span className={`wrapper ${openAccordionList.includes(course.id) ? 'open' : ''}`}>
-                  <div className="details">
-                    <h4>Instituição: <span>{course.institution}</span></h4>
-                    <p>{course.description}</p>
-                  </div>
-                  </span>
-                  <span onClick={()=>handleAccordionClick(course.id)} className="clicker">+</span>
-                </div>
-              })
-            }
-          </span>
-
-          <span className="infos-section">
-            <h2>Habilidades e competências:</h2>
-            <hr/>
-            <div className='skills'>
+            <span className="infos-section">
+              <h2>Experiências profissionais:</h2>
+              <hr/>
+                  
+                                          
               {
-                user?.skills.map((skill) => (
-                  <div key={skill.id} className='skill'>
-                    <h3>{skill.name}</h3>
-                  </div>
-                ))
-              }
-            </div>            
-          </span>
-          <span className="infos-section">
-            <h2>Certificados e documentos:</h2>
-            <hr/>
-            <div className='documents'>
-
-              {
-                user?.documents.map((document, index) => (
-                  <a href={document.document} target="_blank">
-                    <div className='document' ref={(el) => (documentsRef.current[index] = el)}>
-                      <div className='document-text'>
-                        <h3>{document.name}</h3>
-                        <p>{document.description}</p>
-                      </div>
-                      <img src={pdfIcon} alt="imagem de um pdf" />
+                user?.experiences.map((experience) => (
+                  <div key={experience.id} className="experience">
+                  <h3>{experience.title}<span>{experience.from} — {experience.to}</span></h3>
+                  <span className={`wrapper ${openAccordionList.includes(experience.id) ? 'open' : ''}`}>
+                    <div className="details">
+                      <h4>Companhia: <span>{experience.company}</span></h4>
+                      <p>{experience.description}</p>
                     </div>
-                  </a>
+                    </span>
+                    <span onClick={()=>handleAccordionClick(experience.id)} className="clicker">+</span>
+                  </div>
                 ))
               }
-            </div>
-          </span>          
-        </UserInfos>
-      </div>
-    </ContainerProfile>
+            </span>
+
+            <span className="infos-section">
+              <h2>Formações Acadêmicas:</h2>
+              <hr/>
+              {
+                user?.courses.map((course) => {
+                  
+                  return <div key={course.id} className="experience">
+                    <h3>{course.name}<span>{course.from} { course.to && `— ${course.to}`}</span></h3>
+                    <span className={`wrapper ${openAccordionList.includes(course.id) ? 'open' : ''}`}>
+                    <div className="details">
+                      <h4>Instituição: <span>{course.institution}</span></h4>
+                      <p>{course.description}</p>
+                    </div>
+                    </span>
+                    <span onClick={()=>handleAccordionClick(course.id)} className="clicker">+</span>
+                  </div>
+                })
+              }
+            </span>
+
+            <span className="infos-section">
+              <h2>Habilidades e competências:</h2>
+              <hr/>
+              <div className='skills'>
+                {
+                  user?.skills.map((skill) => (
+                    <div key={skill.id} className='skill'>
+                      <h3>{skill.name}</h3>
+                    </div>
+                  ))
+                }
+              </div>            
+            </span>
+            <span className="infos-section">
+              <h2>Certificados e documentos:</h2>
+              <hr/>
+              <div className='documents'>
+
+                {
+                  user?.documents.map((document, index) => (
+                    <a href={document.document} target="_blank" rel="noreferrer">
+                      <div className='document' ref={(el) => (documentsRef.current[index] = el)}>
+                        <div className='document-text'>
+                          <h3>{document.name}</h3>
+                          <p>{document.description}</p>
+                        </div>
+                        <img src={pdfIcon} alt="imagem de um pdf" />
+                      </div>
+                    </a>
+                  ))
+                }
+              </div>
+            </span>          
+          </UserInfos>
+        </div>
+      </ContainerProfile>
+    </>
   )
 }
